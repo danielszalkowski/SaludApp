5b2f9310b9a63615716395aea1c1b859e098adf7
Daniel Szalkowski <chenano89@gmail.com>
2023-11-07 11:51:07 +0100

    1.0

--- app/src/main/java/com/pmdm/saludapp/MainActivity.kt
+++ app/src/main/java/com/pmdm/saludapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +17,15 @@ class MainActivity : AppCompatActivity() {
         btnHello.setOnClickListener {
             //Log.i("Test", "Botón pulsado")
             val name = etName.text.toString()
-            Toast.makeText(this, "Hola $name!", Toast.LENGTH_LONG).show()
+            //Toast.makeText(this, "Hola $name!", Toast.LENGTH_LONG).show()
+            if (name.isNotEmpty()) {
+                Snackbar.make(btnHello, "Hola $name!", Snackbar.LENGTH_SHORT)
+                    .setAnchorView(btnHello)
+                    .setAction("CLOSE") { etName.setText(null) }
+                    .show()
+            } else {
+                Toast.makeText(this, "Introduce un nombre.", Toast.LENGTH_LONG).show()
+            }
         }
     }
 
